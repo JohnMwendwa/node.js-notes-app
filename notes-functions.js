12f3ee2ca9abb37b2ec1de72fdd70d36956173fc
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-06 23:42:07 +0300

    create colorful log messages

--- notes-functions.js
+++ notes-functions.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const chalk = require('chalk');
+
+//COLOR VARIABLES
+const success = chalk.bgGreen;
+const error = chalk.bgRed;
 
 //Load notes 
 const loadNotes =()=>{
@@ -23,20 +28,20 @@ const addNote = (title,body)=>{
     const notes = loadNotes();
     const duplicateNote = notes.find(note=> note.title === title);
     if(duplicateNote){
-       console.log('Note already exists!!!');
+       console.log(error('Note already exists!!!'));
     }else{
         notes.push({
             title,
             body
         })
         saveNote(notes)
-        console.log('New note saved!!!')
+        console.log(success('New note saved!!!'))
     }
 }
 
 const listNotes =()=>{
     const notes = loadNotes();
-    console.log('Your Notes')
+    console.log(chalk.underline('Your Notes'));
     notes.forEach(note=>console.log(note.title))
 }
 
@@ -45,9 +50,9 @@ const removeNote =(title)=>{
     const remainingNotes = notes.filter(note=>note.title !== title);   
     if(notes > remainingNotes){
         saveNote(notes);
-        console.log('Note removed successfully!')
+        console.log(success('Note removed successfully!'))
     }else{
-        console.log('No such title exits')
+        console.log(error('No such title exits'))
     }
 }
 
