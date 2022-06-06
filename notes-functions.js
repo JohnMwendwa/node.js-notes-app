1ba95ba195886f515b51dca80e542ad4104d0aa6
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-06 22:24:29 +0300

    create addNote function to add a new note in storage

--- notes-functions.js
+++ notes-functions.js
@@ -17,3 +17,19 @@ const saveNote =(note)=>{
     const dataJSON = JSON.stringify(note);
     fs.writeFileSync('notes.json',dataJSON)
 }
+
+// Add a new note
+const addNote = (title,body)=>{
+    const notes = loadNotes();
+    const duplicateNote = notes.find(note=> note.title === title);
+    if(duplicateNote){
+       console.log('Note already exists!!!');
+    }else{
+        notes.push({
+            title,
+            body
+        })
+        saveNote(notes)
+        console.log('New note saved!!!')
+    }
+}
