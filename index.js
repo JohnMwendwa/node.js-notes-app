7da523412a217929bf6a339a29543d4faa817184
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-07 00:19:04 +0300

    create add command to add new note

---
+++ index.js
@@ -0,0 +1,18 @@
+const yargs = require('yargs/yargs');
+const {hideBin} = require('yargs/helpers');
+const {addNote,listNotes,removeNote} = require('./notes-functions')
+const argv = yargs(hideBin(process.argv))
+    .command('add','Add a note',{
+        title:{
+            describe:"Note title",
+            demand:true,
+            type:'string'
+        },
+        body:{
+            describe:"Note body",
+            demand:true,
+            type:'string'
+        }
+    },(argv)=>addNote(argv.title,argv.body))
+    .help()
+    .argv
