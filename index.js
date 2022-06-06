360341b1697fb157008936aefa6664db3ae6ef15
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-07 00:48:27 +0300

    create a read command to read the contents of a single note

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const yargs = require('yargs/yargs');
 const {hideBin} = require('yargs/helpers');
-const {addNote,listNotes,removeNote} = require('./notes-functions')
+const {addNote,readNote,listNotes,removeNote} = require('./notes-functions')
 const argv = yargs(hideBin(process.argv))
     .command('add','Add a note',{
         title:{
@@ -14,6 +14,13 @@ const argv = yargs(hideBin(process.argv))
             type:'string'
         }
     },(argv)=>addNote(argv.title,argv.body))
+    .command('read','Read a note',{
+        title:{
+            describe:'Note title',
+            demand:true,
+            type:'string'
+        }
+    },(argv)=>readNote(argv.title))
     .command('list','List all notes',()=>listNotes())
     .command('remove',"Remove a note",{
         title:{
